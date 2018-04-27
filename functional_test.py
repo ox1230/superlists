5208564d28922cd4e48c3580b27fe9a227a3709e
ox1230 <ox11223300@gmail.com>
2018-04-27 22:15:31 +0900

    첫 기능 테스트 주석 추가 및 unittest 사용

--- functional_test.py
+++ functional_test.py
@@ -1,5 +1,37 @@
 from selenium import webdriver
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+import unittest  
 
-assert 'Django' in browser.title
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        """테스트 시작 전에 수행"""
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)   # 암묵적 대기 -- 3초
+    def tearDown(self):
+        """테스트 후에 시행-- 테스트에 에러가 발생해도 실행된다"""
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        """  """
+        #해당 웹사이트 방문
+        self.browser.get('http://localhost:8000')
+
+        # 타이틀과 헤더가 'To-Do'를 표시
+        self.assertIn('To-Do' ,self.browser.title)  # 다양한 assert
+        self.fail('Finish the test!')  # 강제적 테스트 실패 발생
+
+
+        #작업 추가
+
+        # '공작깃털 사기'라고 텍스트 상자에 입력
+
+        # 엔터키를 치면 페이지 갱신, 작업 목록에 "1: 공작깃털 사기"아이템이 추가 
+
+        # 추가 아이템을 입력할 수 있는 여분의 텍스트 상자 존재
+        # 다시 '공작 깃털을 이용해 그물 만들기"라고 입력
+
+        # 페이지 갱신, 두개의 아이템이 목록에 표시
+
+        #사이트가 입력한 목록을 저장하고 있는가? -- 특정 URL로 접속하면 작업목록이 그대로 있다.
+
+if __name__ == '__main__':
+    unittest.main(warnings= 'ignore')
