dd172dc15e16c4d85ee25fde888d7ddfb155a898
ox1230 <ox11223300@hanmail.net>
2018-04-28 14:46:23 +0900

    github test용

--- lists/views.py
+++ lists/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 
 def home_page(request) -> HttpResponse:
-    return HttpResponse('<html> <title>To-Do lists</title></html>')
+    return HttpResponse("<html> <title>To-Do lists</title></html>")
