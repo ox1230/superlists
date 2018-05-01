ff8c31586f28177504c88d0dbc25cfd40c286d15
ox1230 <ox11223300@gmail.com>
2018-05-01 16:09:59 +0900

    아이템용 모델과 마이그레이션 연계

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,7 @@
+from lists.views import home_page
+from lists.models import Item
 from django.test import TestCase
 from django.urls import resolve
-from lists.views import home_page
 from django.http import HttpRequest
 from django.template.loader import render_to_string
 import re
@@ -39,4 +40,21 @@ class HomePageTest(TestCase):
         """Remove csrf tag from TEXT"""
         return re.sub(r'<[^>]*csrfmiddlewaretoken[^>]*>', '', text)
 
-    
+class ItemModelTest(TestCase):
+
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = '첫 번째 아이템'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = '두 번째 아이템'
+        second_item.save()
+        
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, '첫 번째 아이템')
+        self.assertEqual(second_saved_item.text, '두 번째 아이템')
