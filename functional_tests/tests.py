17fa04ee7f66b50c0bac085de8bd485a14bf13bf
ox1230 <ox11223300@hanmail.net>
2018-05-02 22:56:39 +0900

    functional_tests 변경

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -17,7 +17,7 @@ class NewVisitorTest(LiveServerTestCase):
     
     def test_can_start_a_list_and_retrieve_it_later(self):
         """  """
-        #해당 웹사이트 방문
+        #edith가 해당 웹사이트 방문
         self.browser.get(self.live_server_url)
 
         # 타이틀과 헤더가 'To-Do'를 표시
@@ -33,11 +33,12 @@ class NewVisitorTest(LiveServerTestCase):
 
         # '공작깃털 사기'라고 텍스트 상자에 입력
         inputBox.send_keys('공작깃털 사기')
-
-
         # 엔터키를 치면 페이지 갱신, 작업 목록에 "1: 공작깃털 사기"아이템이 추가 
         inputBox.send_keys(Keys.ENTER)
+        edith_list_url = self.browser.current_url
 
+        self.assertRegex(edith_list_url, '/lists/.+')
+        #assertRegex: 지정 정규표현식과 문자열이 일치하는지 확인
         self.check_for_row_in_list_table('1: 공작깃털 사기')
     
 
@@ -52,8 +53,32 @@ class NewVisitorTest(LiveServerTestCase):
         self.check_for_row_in_list_table("2: 공작깃털을 이용해서 그물 만들기")
         self.check_for_row_in_list_table('1: 공작깃털 사기')
     
-        self.fail('Finish the test!')  # 강제적 테스트 실패 발생
-        #사이트가 입력한 목록을 저장하고 있는가? -- 특정 URL로 접속하면 작업목록이 그대로 있다.
+        #Francis가 사이트에 접속
+        ## 새로운 브라우저 세션을 이용해서 Edith의 정보가 쿠키를 통해 유입되는 것을 방지한다.
+
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+
+        self.assertNotIn('공작깃털 사기', page_text)
+        self.assertNotIn('그물 만들기', page_text)
+
+        #프란시스가 새로운 작업 아이템을 입력한다
+        inputBox = self.browser.find_element_by_id('id_new_item')
+        
+        inputBox.send_keys('우유 사기')
+        inputBox.send_keys(Keys.ENTER)
+
+        #프란시스가 전용 URL을 취득한다
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        #에디스가 입력한 흔적이 없다는 것을 확인한다.
+
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('공작깃털 사기', page_text)
+        self.assertIn(page_text)
+
 
     def check_for_row_in_list_table(self, row_text):
         time.sleep(1)        
