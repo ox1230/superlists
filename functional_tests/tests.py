a026004116b43cf2eb0e8c8f6ce64fe31fee006c
ox1230 <ox11223300@hanmail.net>
2018-05-10 20:48:20 +0900

    레이아웃과 스타일링을 위한 FT변경과 부트스트랩

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -81,6 +81,30 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertNotIn('공작깃털 사기', page_text)
         self.assertIn('우유 사기', page_text)
 
+    def test_layout_and_styling(self):
+        
+        #에디스는 메인페이지를 방문한다
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024,768)
+
+        #입력상자가 가운데에 위치한 것을 본다
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x' ] + inputbox.size['width']/2,
+            512,
+            delta = 10
+        )
+
+        #새로운 리스트를 시작하고 입력상자가 가운데 배치된것을 확인
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width']/2,
+            512,
+            delta = 10
+        )
 
     def check_for_row_in_list_table(self, row_text):
         time.sleep(1)        
