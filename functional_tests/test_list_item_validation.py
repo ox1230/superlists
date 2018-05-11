36e283adb8eb24252a751e3238b8c6d89cb829f7
ox1230 <ox11223300@hanmail.net>
2018-05-11 23:41:24 +0900

    test_cannot_add_empty_list_items 작성

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -10,21 +10,39 @@ import sys
 
 
 class ItemValidationTest(FunctionalTest):
-    @skip
-    def test_cannot_addd_empty_list_items(self):
+    
+    def test_cannot_add_empty_list_items(self):
         #에디스는 메인페이지에 접속해 빈아이템을 실수로 등록하려 한다
 
         # 입력상자가 비어있는 상태에서 엔터키를 누른다
+        self.browser.get(self.server_url)
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
 
         # 페이지가 새로고침되고, 빈아이템을 등록할 수 없다는 에러메세지가 표시된다
-
+        error = self.browser.find_element_by_css_selector('.has-error')
+        self.assertEqual(error.text, '빈 아이템을 등록할 수 없습니다')
+       
         # 다른아이템을 입력하고 정상처리
 
+        self.browser.find_element_by_id('id_new_item').send_keys('우유 사기')
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+
+        self.check_for_row_in_list_table('1: 우유 사기')
+
         # 다시 빈아이템 입력
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
 
         # 에러메세지 표시
+        self.check_for_row_in_list_table('1: 우유 사기')
+        error = self.browser.find_element_by_css_selector('.has-error')
+        self.assertEqual(error.text, '빈 아이템을 등록할 수 없습니다')
 
+        #아이템 입력시 다시 정상 동작
+        self.browser.find_element_by_id('id_new_item').send_keys('차 만들기')
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
 
+        self.check_for_row_in_list_table('1: 우유 사기')
+        self.check_for_row_in_list_table('2: 차 만들기')
 
         self.fail('write me!')
 
